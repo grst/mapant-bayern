59a36275725a486f2d617f82c86f66819433525c
EC2 Default User <ec2-user@ip-172-31-22-83.eu-north-1.compute.internal>
2026-08-03 05:54:59 +0000

    update hashes

--- processing_pipeline/run_allgaeu.sh
+++ processing_pipeline/run_allgaeu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nextflow run grst/mapant-nf -params-file conf/test_allgaeu.yml -profile docker -c conf/c8id.4xlarge.config  -r 7375123 -resume
+nextflow run grst/mapant-nf -params-file conf/test_allgaeu.yml -profile docker -c conf/c8id.4xlarge.config  -r 03297698ddf9a1433ccb54fe37f30fd327d672c4 -resume
